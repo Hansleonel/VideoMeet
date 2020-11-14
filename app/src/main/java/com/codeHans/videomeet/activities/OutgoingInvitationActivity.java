574fb0515998e563670c27bd7c9225e1276f7046
hans <hansleonel@gmail.com>
2020-11-14 02:14:33 -0500

    add VideoMeeting SDK to gradle add Functionality to VideoMeeting add Properties to start the VideoMeeting add Create Room to the users

--- app/src/main/java/com/codeHans/videomeet/activities/OutgoingInvitationActivity.java
+++ app/src/main/java/com/codeHans/videomeet/activities/OutgoingInvitationActivity.java
@@ -24,13 +24,19 @@ import com.codeHans.videomeet.utilities.Constants;
 import com.codeHans.videomeet.utilities.PreferenceManager;
 import com.google.firebase.iid.FirebaseInstanceId;
 
+import org.jitsi.meet.sdk.JitsiMeetActivity;
+import org.jitsi.meet.sdk.JitsiMeetConferenceOptions;
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+import java.net.URL;
+import java.util.UUID;
 
 public class OutgoingInvitationActivity extends AppCompatActivity {
 
     private PreferenceManager preferenceManager;
     private String inviterToken = null;
+    String meetingRoom = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -92,6 +98,11 @@ public class OutgoingInvitationActivity extends AppCompatActivity {
             data.put(Constants.KEY_LAST_NAME, preferenceManager.getString(Constants.KEY_LAST_NAME));
             data.put(Constants.KEY_EMAIL, preferenceManager.getString(Constants.KEY_EMAIL));
             data.put(Constants.REMOTE_MSG_INVITER_TOKEN, inviterToken);
+
+            meetingRoom =
+                    preferenceManager.getString(Constants.KEY_USER_ID) + "_" +
+                            UUID.randomUUID().toString().substring(0, 5);
+            data.put(Constants.REMOTE_MSG_MEETING_ROOM, meetingRoom);
 
             body.put(Constants.REMOTE_MSG_DATA, data);
             body.put(Constants.REMOTE_MSG_REGISTRATION_IDS, tokens);
@@ -161,6 +172,24 @@ public class OutgoingInvitationActivity extends AppCompatActivity {
             if (type != null) {
                 if (type.equals(Constants.REMOTE_MSG_INVITATION_ACCEPTED)) {
                     Toast.makeText(context, "Invitation Accepted", Toast.LENGTH_LONG).show();
+
+                    try {
+
+                        URL serverURL = new URL("https://meet.jit.si");
+                        JitsiMeetConferenceOptions conferenceOptions =
+                                new JitsiMeetConferenceOptions.Builder()
+                                        .setServerURL(serverURL)
+                                        .setWelcomePageEnabled(false)
+                                        .setRoom(meetingRoom)
+                                        .build();
+
+                        JitsiMeetActivity.launch(OutgoingInvitationActivity.this, conferenceOptions);
+                        finish();
+                    } catch (Exception exception) {
+                        Toast.makeText(context, exception.getMessage(), Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+
                 } else if (type.equals(Constants.REMOTE_MSG_INVITATION_REJECTED)) {
                     Toast.makeText(context, "Invitation Rejected", Toast.LENGTH_SHORT).show();
                     finish();
